8c27a1d67df7a1e7e089ccdb025d5260bd59f11f
gtmbuddha <nimesgautam@gmail.com>
2023-06-16 12:50:50 +0545

    implemented actual logic for create, update, delete post

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,37 +1,119 @@
-import { Controller, Get, Post, Put, Delete, Param } from '@nestjs/common';
-import posts_data from './data';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Body,
+  HttpCode,
+} from '@nestjs/common';
+import blog_data from './data';
+import { Category } from './data';
+import { v4 as uuid } from 'uuid';
 
-@Controller('blog/post')
+@Controller('blog/post/:category')
 export class AppController {
-  @Get('all')
+  @Get('allPosts')
   getAllBlogPosts() {
-    return posts_data;
+    return blog_data.posts;
+  }
+
+  @Get('all')
+  getAllBlogPostsByCategory(@Param('category') category: string) {
+    const cat = category === 'dog' ? Category.DOG : Category.CAT;
+    return blog_data.posts.filter((post) => post.category === cat);
   }
 
   @Get(':id')
-  getBlogPostById(@Param('id') id: string) {
-    const data = posts_data.filter((data) => data.id === id);
+  getBlogPostById(
+    @Param('id') id: string,
+    @Param('category') category: string,
+  ) {
+    const cat = category === 'dog' ? Category.DOG : Category.CAT;
+    const data = blog_data.posts.filter(
+      (post) => post.category === cat && post.id === id,
+    );
+
     return data;
   }
 
   @Post('')
-  createNewBlogPost() {
-    return 'Created';
+  createNewBlogPost(
+    @Body()
+    {
+      title,
+      author,
+      body,
+    }: {
+      title: string;
+      author: string;
+      body: string;
+    },
+    @Param('category') category: string,
+  ) {
+    const newBlogPost = {
+      id: uuid(),
+      title,
+      created_at: new Date(),
+      updated_at: new Date(),
+      author,
+      body,
+      category: category === 'dog' ? Category.DOG : Category.CAT,
+    };
+
+    blog_data.posts.push(newBlogPost);
+
+    return newBlogPost;
   }
 
   @Put(':id')
-  updateBlogPostById() {
-    return 'Updated';
+  updateBlogPostById(
+    @Body()
+    body: {
+      title: string;
+      body: string;
+    },
+    @Param('id') id: string,
+    @Param('category') category: string,
+  ) {
+    console.log(body);
+    const cat = category === 'dog' ? Category.DOG : Category.CAT;
+    const data = blog_data.posts
+      .filter((post) => post.category === cat)
+      .find((post) => post.id === id);
+    console.log(data);
+
+    if (!data) return;
+
+    const dataIndex = blog_data.posts.findIndex((post) => post.id === data.id);
+
+    blog_data.posts[dataIndex] = {
+      ...blog_data.posts[dataIndex],
+      ...body,
+    };
+
+    return blog_data.posts[dataIndex];
   }
 
+  @HttpCode(204)
   @Delete(':id')
-  deleteBlogPostById() {
-    return 'Deleted';
+  deleteBlogPostById(@Param('id') id: string) {
+    const postIndex = blog_data.posts.findIndex((post) => post.id === id);
+
+    if (postIndex === -1) return;
+
+    blog_data.posts.splice(postIndex, 1);
+
+    // return;
   }
 }
 
-//  get    /blog/post/all
-//  get    /blog/post/:id
-//  post   /blog/post
-//  put   /blog/post/:id
-//  delete  /blog/post/:id
+//  category = dogs, cats, etc.
+
+//  get    /blog/post/:category/allPosts
+//  get    /blog/post/:category/all
+//  get    /blog/post/:category/:id
+//  post   /blog/post/:category/
+//  put   /blog/post/:category/:id
+//  delete  /blog/post/:category/:id
--- src/data.ts
+++ src/data.ts
@@ -1,46 +1,62 @@
+export enum Category {
+  DOG = 'dog',
+  CAT = 'cat',
+}
+
 type post_data = {
   id: string;
-  created_date: Date;
-  updated_date: Date;
+  created_at: Date;
+  updated_at: Date;
   title: string;
   author: string;
   body: string;
+  category: Category;
 };
 
-const posts_data: Array<post_data> = [
-  {
-    id: '1',
-    title: 'Powerful Trading Tools and Features for Experienced Investors',
-    created_date: new Date(),
-    updated_date: new Date(),
-    author: 'Nimesh Gautam',
-    body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus mollitia, vel illum quis perferendis ducimus quibusdam harum iste fuga exercitationem at quae accusantium placeat, possimus alias. Temporibus veniam provident laborum necessitatibus ullam sit est sapiente illum excepturi dignissimos. Similique repellat harum incidunt itaque dolorum dolore reiciendis facilis, exercitationem ut explicabo.',
-  },
-  {
-    id: '2',
-    title: '2023 Indian Premier League',
-    created_date: new Date(),
-    updated_date: new Date(),
-    author: 'Nimesh Gautam',
-    body: 'The 2023 Indian Premier League (also known as Tata IPL 2023 for sponsorship reasons and sometimes referred to as IPL 2023 or IPL 16) was the 16th season of the Indian Premier League, a franchise Twenty20 cricket league in India. It is organised by the Board of Control for Cricket in India.[1]',
-  },
-  {
-    id: '3',
-    title: 'Powerful Trading Tools and Features for Experienced Investors',
-    created_date: new Date(),
-    updated_date: new Date(),
-    author: 'Nimesh Gautam',
-    body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus mollitia, vel illum quis perferendis ducimus quibusdam harum iste fuga exercitationem at quae accusantium placeat, possimus alias. Temporibus veniam provident laborum necessitatibus ullam sit est sapiente illum excepturi dignissimos. Similique repellat harum incidunt itaque dolorum dolore reiciendis facilis, exercitationem ut explicabo.',
-  },
-  {
-    id: '4',
-    title: 'Powerful Trading Tools and Features for Experienced Investors',
-    created_date: new Date(),
-    updated_date: new Date(),
-    author: 'Nimesh Gautam',
-    body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus mollitia, vel illum quis perferendis ducimus quibusdam harum iste fuga exercitationem at quae accusantium placeat, possimus alias. Temporibus veniam provident laborum necessitatibus ullam sit est sapiente illum excepturi dignissimos. Similique repellat harum incidunt itaque dolorum dolore reiciendis facilis, exercitationem ut explicabo.',
-  },
-];
+type Data = {
+  posts: Array<post_data>;
+};
+
+const blog_data: Data = {
+  posts: [
+    {
+      id: '1',
+      title: 'Powerful Trading Tools and Features for Experienced Investors',
+      created_at: new Date(),
+      updated_at: new Date(),
+      author: 'Nimesh Gautam',
+      body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus mollitia, vel illum quis perferendis ducimus quibusdam harum iste fuga exercitationem at quae accusantium placeat, possimus alias. Temporibus veniam provident laborum necessitatibus ullam sit est sapiente illum excepturi dignissimos. Similique repellat harum incidunt itaque dolorum dolore reiciendis facilis, exercitationem ut explicabo.',
+      category: Category.DOG,
+    },
+    {
+      id: '2',
+      title: '2023 Indian Premier League',
+      created_at: new Date(),
+      updated_at: new Date(),
+      author: 'Nimesh Gautam',
+      body: 'The 2023 Indian Premier League (also known as Tata IPL 2023 for sponsorship reasons and sometimes referred to as IPL 2023 or IPL 16) was the 16th season of the Indian Premier League, a franchise Twenty20 cricket league in India. It is organised by the Board of Control for Cricket in India.[1]',
+      category: Category.DOG,
+    },
+    {
+      id: '3',
+      title: 'Powerful Trading Tools and Features for Experienced Investors',
+      created_at: new Date(),
+      updated_at: new Date(),
+      author: 'Nimesh Gautam',
+      body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus mollitia, vel illum quis perferendis ducimus quibusdam harum iste fuga exercitationem at quae accusantium placeat, possimus alias. Temporibus veniam provident laborum necessitatibus ullam sit est sapiente illum excepturi dignissimos. Similique repellat harum incidunt itaque dolorum dolore reiciendis facilis, exercitationem ut explicabo.',
+      category: Category.CAT,
+    },
+    {
+      id: '4',
+      title: 'Powerful Trading Tools and Features for Experienced Investors',
+      created_at: new Date(),
+      updated_at: new Date(),
+      author: 'Nimesh Gautam',
+      body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus mollitia, vel illum quis perferendis ducimus quibusdam harum iste fuga exercitationem at quae accusantium placeat, possimus alias. Temporibus veniam provident laborum necessitatibus ullam sit est sapiente illum excepturi dignissimos. Similique repellat harum incidunt itaque dolorum dolore reiciendis facilis, exercitationem ut explicabo.',
+      category: Category.CAT,
+    },
+  ],
+};
 
 // let data_category = [
 //   'Company',
@@ -51,5 +67,5 @@ const posts_data: Array<post_data> = [
 //   'Work',
 // ];
 
-export default posts_data;
+export default blog_data;
 // export { data_category };
