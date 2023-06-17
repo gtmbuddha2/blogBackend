9a44d423d6891a5df7a7258bae8590521a1c4381
gtmbuddha <nimesgautam@gmail.com>
2023-06-18 00:33:54 +0545

    transforming through interceptors completed

--- src/custom.interceptor.ts
+++
@@ -1,8 +0,0 @@
-import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
-import { Observable } from 'rxjs';
-
-class CustomInterceptor implements NestInterceptor {
-  intercept(context: ExecutionContext, handler: CallHandler) {
-    return handler.handle().pipe();
-  }
-}
